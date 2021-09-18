63f4fd6db458fd665295876fc935fb4bdc634a37
21 Sapphire <21sapphiremusic@gmail.com>
2021-09-18 12:19:52 -0500

    init

---
+++ Basics/Ex1.rb
@@ -0,0 +1,36 @@
+# 1
+puts "Matthew " + "Buckman"
+
+# 2
+puts 5678 / 1000
+puts 5678 % 1000 / 100
+puts 5678 % 1000 % 100 / 10
+puts 5678 % 1000 % 100 % 10
+
+# 3
+movies = {:jurassic_park => 1993, :et => 1982, :minority_report => 2002, :bridge_of_spies => 2015}
+puts movies[:jurassic_park]
+puts movies[:et]
+puts movies[:minority_report]
+puts movies[:bridge_of_spies]
+
+# 4
+date_movies = [1993, 1982, 2002, 2015]
+puts date_movies[0]
+puts date_movies[1]
+puts date_movies[2]
+puts date_movies[3]
+
+# 5
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+# 6
+puts 6.5*6.5
+puts 7.567*7.567
+puts 3.22*3.22
+
+# 7
+# On line 2, a closing parenthesis was used instead of a closing curly brace.
